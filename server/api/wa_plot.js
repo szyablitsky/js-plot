5f6c9051d1c1217b91d7a36e20fe546259354bfe
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2020-03-19 23:08:38 +1000

    wa query done

--- server/api/wa_plot.js
+++ server/api/wa_plot.js
@@ -1,6 +1,18 @@
-const ttt = process.env.WA_APP_ID_JS_PLOT
+const waAppID = process.env.WA_APP_ID_JS_PLOT
+
+const WolframAlphaAPI = require('wolfram-alpha-api')
+const waApi = WolframAlphaAPI(waAppID)
 
 exports.waPlot = function (req, res) {
-  res.set('Content-Type', 'application/json');
-  res.send(`{"message":"Hello from the custom server ${ttt}!"}`);
+  const { expression, min, max } = req.query
+  const query = `plot ${expression} for x between ${min} and ${max}`
+  waApi.getFull(query).then((queryresult) => {
+    if (queryresult.success) {
+      res.set('Content-Type', 'application/json')
+      res.send(`{"imageUrl":"${queryresult.pods[1].subpods[0].img.src}"}`)
+    }
+  }).catch((error) => {
+    res.set('Content-Type', 'application/json')
+    res.send(`{"error":"${error}"}`)
+  })
 }
